1d1e095f544dfd585f7a7fc338004600536e7691
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2022-09-02 19:51:01 +0100

    Navbar setup finished

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,18 +1,103 @@
 import Link from 'next/link'
 import React from 'react'
 import ToggleTheme from './ToggleTheme'
+import { Navbar as NavBarComp, Link as UiLink, Text } from "@nextui-org/react"
+import { useTheme } from "@nextui-org/react";
+
 
 const NavBar = () => {
+    const { type } = useTheme();
+
     return (
-        <div className='flex justify-between py-4 mb-10 px-3 bg-slate-500'>
-            <Link href={'/'}>
-                <a className="text-2xl text-cyan-500">
-                    ANNOYMOUS
-                </a>
-            </Link>
-            <ToggleTheme />
-        </div>
+        <NavBarComp isBordered variant="sticky">
+            <NavBarComp.Toggle showIn="xs" />
+            <NavBarComp.Brand
+                css={{
+                    "@xs": {
+                        w: "12%",
+                    },
+                }}
+            >
+                <Text b color="inherit">
+                    <Link href={'/'}>
+                        ANNONYMOUS
+                    </Link>
+                </Text>
+            </NavBarComp.Brand>
+            <NavBarComp.Content
+                activeColor="secondary"
+                variant="highlight-rounded"
+                hideIn="xs"
+            >
+                <NavBarComp.Link href="/account/dashboard">
+                    Dashboard
+                </NavBarComp.Link>
+                <NavBarComp.Link href="/account/dashboard">
+                    Messages
+                </NavBarComp.Link>
+
+            </NavBarComp.Content>
+            {/*  */}
+            <NavBarComp.Content
+            // activeColor={'secondary'}
+            // variant="highlight-rounded"
+            >
+                <NavBarComp.Link
+                    hideIn="xs"
+                    href="/auth/login">
+                    Login
+                </NavBarComp.Link>
+                <NavBarComp.Link href="/auth/register"
+                    hideIn="xs"
+                >
+                    Register
+                </NavBarComp.Link>
+                <NavBarComp.Link href="/"
+                    hideIn="xs"
+                    className={type === 'dark' ? 'p-3 bg-gray-600 rounded-2xl' : 'p-3 bg-gray-100 rounded-2xl'}
+                >
+                    Logout
+                </NavBarComp.Link>
+                <ToggleTheme />
+            </NavBarComp.Content>
+            <NavBarComp.Collapse>
+                <NavBarComp.CollapseItem
+                >
+                    <UiLink href={'/account/messages'}
+                        color="inherit"
+                        css={{
+                            minWidth: "100%",
+                        }}
+                    >
+                        Messages
+                    </UiLink>
+                </NavBarComp.CollapseItem>
+                <NavBarComp.CollapseItem
+                >
+                    <UiLink href={'/account/dashboard'}
+                        color="inherit"
+                        css={{
+                            minWidth: "100%",
+                        }}
+                    >
+                        Dashboard
+                    </UiLink>
+                </NavBarComp.CollapseItem>
+                <NavBarComp.CollapseItem
+                >
+                    <UiLink href={'/'}
+                        color="inherit"
+                        css={{
+                            minWidth: "100%",
+                        }}
+                    >
+                        Logout
+                    </UiLink>
+                </NavBarComp.CollapseItem>
+            </NavBarComp.Collapse>
+        </NavBarComp>
     )
 }
 
 export default NavBar
+
