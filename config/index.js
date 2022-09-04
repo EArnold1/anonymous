ffc4b1e04e38283e7d47f13b0bdeb08b9ba340dd
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2022-09-04 19:37:07 +0100

    updated next url name

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 
-export const NEXT_URL = process.env.NEXT_PUBLIC_NEXT_CLIENT_URL || 'http://localhost:3000'
+export const NEXT_URL = process.env.NEXT_PUBLIC_CLIENT_URL || 'http://localhost:3000'
