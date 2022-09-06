fdf0731506791398b5f64e49c7b524db295cad0d
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2022-09-06 16:48:27 +0100

    onesignal push notification setup complete

--- notification/notification.js
+++ notification/notification.js
@@ -3,7 +3,6 @@ import OneSignal from 'react-onesignal';
 const runNotification = async (username) => {
     // await OneSignal.init({ appId: process.env.NEXT_PUBLIC_NOTIFICATION_KEY, allowLocalhostAsSecureOrigin: true });
     await OneSignal.init({ appId: process.env.NEXT_PUBLIC_NOTIFICATION_KEY });
-    console.log(process.env.NEXT_PUBLIC_NOTIFICATION_KEY)
 
     await OneSignal.showSlidedownPrompt();
 
